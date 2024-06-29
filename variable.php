9f52d0a8e7e69cfd8de209240dc097dac12b9a40
youhannamina123 <youhannamina123@gmail.com>
2024-06-28 23:06:47 -0400

    variable dans main branche

---
+++ variable.php
@@ -0,0 +1,10 @@
+<?php
+$nom = 'mina';
+$nomFamille = 'ghobrial';
+$noteMath = 10; 
+$noteFrancais = 15;
+$noteMoyenne = ($noteMath + $noteFrancais) / 2; 
+// echo $nom.' '.$nomFamille.' : '. $noteMoyenne;
+echo "$nom $nomFamille : $noteMoyenne";
+echo "\nconfirme";//oblige de mettre double guilemet prend en consideration les variables
+?>
